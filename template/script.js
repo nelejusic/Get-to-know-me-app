c5986807bccf5e4bc333ef3f3a1229b1d528360c
nelethemamba998 <72813469+nelethemamba998@users.noreply.github.com>
2021-11-18 00:13:03 +0100

    Added template folder, changed html, css, added some anchors and etc

---
+++ template/script.js
@@ -0,0 +1,3 @@
+var intro =
+  " This website will help you to get to know me just a little bit. You'll get to know who I am, what my skillset is and what kind of technologies, and projects I've worked on ";
+document.getElementById("intro").innerHTML = intro;
