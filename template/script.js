2bdf9e3f2e76a0f91abb777d5cd4ecfc398bb5b9
nelethemamba998 <72813469+nelethemamba998@users.noreply.github.com>
2021-11-18 12:24:37 +0100

    Added content to the about me section

--- template/script.js
+++ template/script.js
@@ -1,3 +1,14 @@
 var intro =
   " This website will help you to get to know me just a little bit. You'll get to know who I am, what my skillset is and what kind of technologies, and projects I've worked on ";
 document.getElementById("intro").innerHTML = intro;
+let aboutMe =
+  "My name is Nedžib Jusić, I am a software developer from Tuzla. I am an educated software developer that has a becherlors degree in IT. I am currently based in Sarajevo. ";
+document.getElementById("about-me").innerHTML = aboutMe;
+
+let expertise =
+  "I am a full stack developer. Working with technologies such as Angular and node.js. I have closely worked with MySql, MariaDB, knex.js, google API, Java, Java Spring Boot, Angular material and etc.";
+
+document.getElementById("expertise").innerHTML = expertise;
+
+let whatILove ="I consider myself as a very social person I enjoj working with others and I love people. I love anything that includes computers so that means I love exploring the hardware, software of the computer, video games and coding";
+document.getElementById("what-i-love").innerHTML  = whatILove
